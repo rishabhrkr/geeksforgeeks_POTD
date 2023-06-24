c00472dc81aa2f8b6e03441b2541d07175c5ca2a
Rishabh Kumar <124022401+rishabhrkr@users.noreply.github.com>
2023-06-24 18:11:27 +0530

    Create Prefix match with other strings.cpp

---
+++ Prefix match with other strings.cpp
@@ -0,0 +1,17 @@
+class Solution{   
+public:
+    int klengthpref(string arr[], int n, int k, string str){    
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            int ok = 1;
+            for(int j = 0; j < k; j++){
+                if(arr[i][j] != str[j])
+                    ok = 0;
+            }
+            
+            count += ok;
+        }
+        
+        return count;
+    }
+};
